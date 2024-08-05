5691fec8ea94ef93952413892440e33a75d1bb45
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-08-05 11:03:17 +0530

    .

--- src/lib/Profiles/Profiles.svelte
+++ src/lib/Profiles/Profiles.svelte
@@ -2,7 +2,7 @@
   import { onMount } from "svelte";
   const fetchData = async () => {
     try {
-      const response = await fetch("/api/fetchData");
+      const response = await fetch("/api/secret");
       if (!response.ok) throw new Error("Network response was not ok");
       const data = await response.json();
       console.log(data);
