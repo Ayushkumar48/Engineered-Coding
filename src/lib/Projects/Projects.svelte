c3a58c968e1dc9889fa834f4e164d88f4dfdc5c0
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-08-14 15:52:41 +0530

    updated project

--- src/lib/Projects/Projects.svelte
+++ src/lib/Projects/Projects.svelte
@@ -11,12 +11,12 @@
         "A classic HTML, CSS, and JavaScript Tic-Tac-Toe game where two players mark X or O on a 3x3 grid, aiming for three marks in a row horizontally, vertically, or diagonally to win.",
     },
     {
-      url: "https://battleshipthegame.vercel.app/",
-      imgSrc: "./Images/Battleship.png",
-      imgAlt: "Battleship",
-      title: "BattleShip Game",
+      url: "https://pokeden.vercel.app/",
+      imgSrc: "./Images/PokeDen.png",
+      imgAlt: "PokeDen",
+      title: "PokeDen",
       description:
-        "The Battleship game uses a 4x4 grid with hidden ship and water images. Players reveal cells, aiming to uncover four out of five ships in fewer than eight clicks to achieve victory.",
+        "Developed using SvelteKit and Supabase, featuring user account creation and dynamic Pokémon profiles. A nostalgic platform where users can share and personalize their favorite Pokémon profiles.",
     },
     {
       url: "https://bigtrivia.vercel.app/",
