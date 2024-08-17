99aeefcddf99e44591349ac114c21546471b3476
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-08-17 18:49:48 +0530

    updated a project

--- src/lib/Projects/store.js
+++ src/lib/Projects/store.js
@@ -1,12 +1,12 @@
 import { readable } from "svelte/store";
 export let projectsData = readable([
   {
-    url: "https://xotictactoe.vercel.app/",
-    imgSrc: "./Images/tic-tac-toe.png",
-    imgAlt: "Tic-Tac-Toe",
-    title: "Tic Tac Toe Game",
+    url: "https://torbin.vercel.app/",
+    imgSrc: "./Images/TorbinTechStore.png",
+    imgAlt: "torbintechstore",
+    title: "Torbin Tech Store",
     description:
-      "A classic HTML, CSS, and JavaScript Tic-Tac-Toe game where two players mark X or O on a 3x3 grid, aiming for three marks in a row horizontally, vertically, or diagonally to win.",
+      "Developed an e-commerce platform using SvelteKit, Tailwind CSS, and MongoDB, featuring dynamic product listings, secure user authentication, and seamless checkout.",
   },
   {
     url: "https://pokeden.vercel.app/",
@@ -17,11 +17,11 @@ export let projectsData = readable([
       "Developed using SvelteKit and Supabase, featuring user account creation and dynamic Pokémon profiles. A nostalgic platform where users can share and personalize their favorite Pokémon profiles.",
   },
   {
-    url: "https://bigtrivia.vercel.app/",
-    imgSrc: "./Images/Big-Trivia.png",
-    imgAlt: "BigTrivia",
-    title: "Big Trivia",
+    url: "https://xotictactoe.vercel.app/",
+    imgSrc: "./Images/tic-tac-toe.png",
+    imgAlt: "Tic-Tac-Toe",
+    title: "Tic Tac Toe Game",
     description:
-      "Big Trivia is a Svelte-based game with Tailwind and Flowbite UI, testing players on diverse topics. Offers an responsive interface for an interactive trivia experience.",
+      "A classic HTML, CSS, and JavaScript Tic-Tac-Toe game where two players mark X or O on a 3x3 grid, aiming for three marks in a row horizontally, vertically, or diagonally to win.",
   },
 ]);
